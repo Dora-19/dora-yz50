f52e19cdeb11171c299ee9a6d9ed40f77d358adb
Dora-19 <doraalkan@yahoo.com>
2026-08-29 03:17:01 -0400

    all tasks

--- week_2/neuralnets/n_mlp.py
+++ week_2/neuralnets/n_mlp.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from graphviz import Digraph
+import random
 
 class Value:
     
@@ -40,7 +41,7 @@ class Value:
         
         return output
     
-    def _pow__(self, other):   #YENI
+    def __pow__(self, other):   #YENI
         assert isinstance(other, (int, float)), "only supporting int/float powers for now"
         output = Value(self.data**other, (self,), f"**{other}")
 
@@ -50,16 +51,19 @@ class Value:
 
         return output
     
-    def _rmul__(self, other):  #YENI
+    def __radd__(self, other): #YENI
+        return self + other 
+
+    def __rmul__(self, other):  #YENI
         return self * other
     
-    def _truediv__(self, other):  #YENI
+    def __truediv__(self, other):  #YENI
         return self * other**-1
     
-    def _neg__(self):  #YENI
+    def __neg__(self):  #YENI
         return self * -1
     
-    def _sub__(self, other):   #YENI
+    def __sub__(self, other):   #YENI
         return self + (-other)
     
     def exp(self):
@@ -134,58 +138,78 @@ def draw_dot(root):
 
 
 
-# inputs x1,x2
-x1 = Value(2.0, label='x1')
-x2 = Value(0.0, label='x2')
+class Neuron:
+  
+  def __init__(self, nin):
+    self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
+    self.b = Value(random.uniform(-1,1))
+  
+  def __call__(self, x):
+    # w * x + b
+    act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b)
+    out = act.tanh()
+    return out
+  
+  def parameters(self):
+    return self.w + [self.b]
 
-# weights w1,w2
-w1 = Value(-3.0, label='w1')
-w2 = Value(1.0, label='w2')
 
-# bias of the neuron
-b = Value(6.8813735870195432, label='b')
+class Layer:
+  
+  def __init__(self, nin, nout):
+    self.neurons = [Neuron(nin) for _ in range(nout)]
+  
+  def __call__(self, x):
+    outs = [n(x) for n in self.neurons]
+    return outs[0] if len(outs) == 1 else outs
+  
+  def parameters(self):
+    return [p for neuron in self.neurons for p in neuron.parameters()]
 
-# x1*w1 + x2*w2 + b
-x1w1 = x1*w1; x1w1.label = 'x1*w1'
-x2w2 = x2*w2; x2w2.label = 'x2*w2'
-x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1*w1 + x2*w2'
-n = x1w1x2w2 + b; n.label = 'n'
 
-#tanh implement
-# o = n.tanh(); o.label = 'o'   #BUNU DEGISTIRIYORUZ YENI YAZDIGIMIZ KOMPONENTLERLE
+class MLP:
+  
+  def __init__(self, nin, nouts):
+    sz = [nin] + nouts
+    self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+  
+  def __call__(self, x):
+    for layer in self.layers:
+      x = layer(x)
+    return x
+  
+  def parameters(self):
+    return [p for layer in self.layers for p in layer.parameters()]
+  
 
-e = (2*n).exp()
-o = (e - 1) / (e + 1)
+# x = [2.0, 3.0, -1.0]
+n = MLP(3, [4, 4, 1]) # 3 inputs, 2 hidden layers of width 4, and 1 output
+# draw_dot(n(x)).render('week_2/neuralnets/mlp_neuron.gv', view=True)
 
-# o.grad = 1.0
-# o._backward()
-# n._backward()
+xs = [
+  [2.0, 3.0, -1.0],
+  [3.0, -1.0, 0.5],
+  [0.5, 1.0, 1.0],
+  [1.0, 1.0, -1.0],
+]
+ys = [1.0, -1.0, -1.0, 1.0] # desired targets
+# [(yout - ygt)**2 for yout, ygt in zip(ypred, ys)] #MSE loss
 
-# b._backward
-# x1w1x2w2._backward()
+for k in range(20):
 
-# x2w2._backward()
-# x1w1._backward()
+    #forward pass
+    ypred = [n(x) for x in xs]
+    loss = sum([(yout - ygt)**2 for yout, ygt in zip(ypred, ys)])
 
-# o.grad = 1.0
+    #backward pass
+    for p in n.parameters():
+        p.grad = 0.0
+    loss.backward()
 
-# #topological order
-# topo = []
-# visited = set()
-# def build_topo(v):
-#   if v not in visited:
-#     visited.add(v)
-#     for child in v._prev:
-#       build_topo(child)
-#     topo.append(v)
-# build_topo(o)
+    #uypdate
+    for p in n.parameters():
+        p.data += -0.05 * p.grad  # gradient descent
 
-# for node in reversed(topo):
-#   node._backward()
-
-# print(topo)
-
-o.backward()
-
-dot = draw_dot(o)
-dot.render('graph', view=True)
+    print(k, loss.data)
+    
+# draw_dot(loss).render('week_2/neuralnets/mlp_loss.gv', view=True)
